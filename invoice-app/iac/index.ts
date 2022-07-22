24f85bfe5507b748e6ce69c177f71b80a5f9f047
vyrwu <dev.anowak@gmail.com>
2022-07-22 20:41:38 +0200

    feat: Split apps into domain-driven namespaces
    Separates application deployments into separate namespaces to improve
    segregation of concerns, and allow more granular access and network
    control over the resources.

--- invoice-app/iac/index.ts
+++ invoice-app/iac/index.ts
@@ -1,32 +1,95 @@
+import * as pulumi from '@pulumi/pulumi'
 import * as k8s from '@pulumi/kubernetes'
+
+const commonAnnotations = {
+  // Stack name is an environment name
+  'pleo.io/environment': pulumi.getStack(),
+  'pleo.io/domain': 'invoices',
+}
 
 const appLabels = {
   app: 'invoice-app',
-  environment: 'dev',
-  domain: 'payments',
 }
-const deployment = new k8s.apps.v1.Deployment('invoice-app', {
+
+// TODO: RBAC and Namespace management should probably be managed by SRE
+// TODO: Can be more DRY (move into shared Pulumi module)
+const ns: k8s.core.v1.Namespace = new k8s.core.v1.Namespace('invoices', {
   metadata: {
-    name: 'invoice-app',
+    name: 'invoices',
+    annotations: commonAnnotations,
   },
-  spec: {
-    replicas: 3,
-    selector: { matchLabels: appLabels },
-    template: {
-      metadata: { labels: appLabels },
-      spec: {
-        containers: [
-          {
-            name: 'main',
-            image: 'vyrwu/invoice-app:feat-ano-code-challenge',
-            imagePullPolicy: 'Always',
+})
+
+// App management - to be controlled by developers
+const deployment = new k8s.apps.v1.Deployment(
+  'invoice-app',
+  {
+    metadata: {
+      name: 'invoice-app',
+      namespace: ns.metadata.name,
+      labels: appLabels,
+      annotations: commonAnnotations,
+    },
+    spec: {
+      replicas: 3,
+      selector: {
+        matchLabels: appLabels,
+      },
+      template: {
+        metadata: {
+          namespace: ns.metadata.name,
+          labels: appLabels,
+          annotations: commonAnnotations,
+        },
+        spec: {
+          containers: [
+            {
+              name: 'main',
+              image: 'vyrwu/invoice-app:feat-ano-code-challenge',
+              imagePullPolicy: 'Always',
+              ports: [
+                {
+                  name: 'http',
+                  containerPort: 8081,
+                },
+              ],
+            },
+          ],
+          securityContext: {
+            runAsUser: 10001,
           },
-        ],
-        securityContext: {
-          runAsUser: 10001,
         },
       },
     },
   },
+  {
+    customTimeouts: {
+      create: '2m',
+    },
+  }
+)
+
+// This allows 'invoice-app` to discover `payment-provider` in another namespace.
+// It's not enought to make the connection work - `payment-provider` must explicitly
+// allow ingress access from `invoice-app` via NetworkPolicy
+new k8s.core.v1.Service('payment-provider-dns', {
+  metadata: {
+    name: 'payment-provider',
+    namespace: ns.metadata.name,
+    labels: appLabels,
+    annotations: commonAnnotations,
+  },
+  spec: {
+    type: 'ExternalName',
+    externalName: `payment-provider.payments.svc.cluster.local`, // TODO: Consume namespace and app from Stack outputs
+    ports: [
+      {
+        name: 'http',
+        port: 80,
+        targetPort: 80,
+      },
+    ],
+  },
 })
+
 export const name = deployment.metadata.name
--- payment-provider/iac/index.ts
+++ payment-provider/iac/index.ts
@@ -1,32 +1,102 @@
+import * as pulumi from '@pulumi/pulumi'
 import * as k8s from '@pulumi/kubernetes'
+
+const commonAnnotations = {
+  // Stack name is an environment name
+  'pleo.io/environment': pulumi.getStack(),
+  'pleo.io/domain': 'payments',
+}
 
 const appLabels = {
   app: 'payment-provider',
-  environment: 'dev',
-  domain: 'payments',
 }
-const deployment = new k8s.apps.v1.Deployment('payment-provider', {
+
+// TODO: RBAC and Namespace management should probably be managed by SRE
+// TODO: Can be more DRY (move into shared Pulumi module)
+const ns: k8s.core.v1.Namespace = new k8s.core.v1.Namespace('payments', {
   metadata: {
-    name: 'payment-provider',
+    name: 'payments',
+    annotations: commonAnnotations,
   },
-  spec: {
-    replicas: 3,
-    selector: { matchLabels: appLabels },
-    template: {
-      metadata: { labels: appLabels },
-      spec: {
-        containers: [
-          {
-            name: 'main',
-            image: 'vyrwu/invoice-app:feat-ano-code-challenge',
-            imagePullPolicy: 'Always',
+})
+
+// App management - to be controlled by developers
+const deployment = new k8s.apps.v1.Deployment(
+  'payment-provider',
+  {
+    metadata: {
+      name: 'payment-provider',
+      namespace: ns.metadata.name,
+      labels: {
+        app: 'payment-provider',
+      },
+      annotations: commonAnnotations,
+    },
+    spec: {
+      replicas: 3,
+      selector: {
+        matchLabels: appLabels,
+      },
+      template: {
+        metadata: {
+          namespace: ns.metadata.name,
+          labels: appLabels,
+          annotations: commonAnnotations,
+        },
+        spec: {
+          containers: [
+            {
+              name: 'main',
+              image: 'vyrwu/payment-provider:feat-ano-code-challenge',
+              imagePullPolicy: 'Always',
+              ports: [
+                {
+                  name: 'http',
+                  containerPort: 8082,
+                },
+              ],
+            },
+          ],
+          securityContext: {
+            runAsUser: 10001,
           },
-        ],
-        securityContext: {
-          runAsUser: 10001,
         },
       },
     },
   },
-})
+  {
+    customTimeouts: {
+      create: '2m',
+    },
+  }
+)
+
+new k8s.core.v1.Service(
+  'payment-provider',
+  {
+    metadata: {
+      name: 'payment-provider',
+      namespace: ns.metadata.name,
+      labels: appLabels,
+      annotations: commonAnnotations,
+    },
+    spec: {
+      type: 'ClusterIP',
+      selector: appLabels,
+      ports: [
+        {
+          name: 'http',
+          port: 80,
+          targetPort: 8082, // TODO: Consume from payments stack outputs
+        },
+      ],
+    },
+  },
+  {
+    customTimeouts: {
+      create: '2m',
+    },
+  }
+)
+
 export const name = deployment.metadata.name
