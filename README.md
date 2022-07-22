db00aba51a3f38279c2899998a194c470a09b9b3
vyrwu <dev.anowak@gmail.com>
2022-07-22 20:52:24 +0200

    feat: Add Roles and Role Bindings for developers
    Developers should only have access to their respective domain Namespaces.
    This introduces Roles granting developers access to commonly used,
    namespaced resources, and binds them to a Group that ideally should
    match the assumed AWS IAM role.

--- README.md
+++ README.md
@@ -34,3 +34,13 @@ to Kubernetes, the were crash looping (with surprisingly expressive error messag
 In this case, the app did not require any root access, so Dockerfile was changed to create a new non-root user and
 switch to it before running the app binary. Additionally, I slimmed down the image size by removing unnessesary build
 materials, which is a common practice for Go applications.
+
+## Part 3 - Solution
+- [ ] 3.1 Feel free to express your thoughts and share your experiences with real-world examples you worked with in the past. 
+- [ ] 3.2 What would you do to improve this setup and make it "production ready"?
+- [x] 3.3 There are 2 microservices that are maintained by 2 different teams. Each team should have access only to their service inside the cluster. How would you approach this?
+- [ ] 3.4 How would you prevent other services running in the cluster to communicate to `payment-provider`?
+
+Regarding 3.3, there are many ways to achieve this. In this example, I have separated apps into isolated namespaces,
+and via Kubernetes RBAC introduced roles limitting developers actions based on the role they assume via AWS IAM. This
+can also be achieved using simple User accounts with rotating credentials, but it is often not a compliant solution.
--- payment-provider/iac/index.ts
+++ payment-provider/iac/index.ts
@@ -20,6 +20,56 @@ const ns: k8s.core.v1.Namespace = new k8s.core.v1.Namespace('payments', {
   },
 })
 
+const r: k8s.rbac.v1.Role = new k8s.rbac.v1.Role('payments-admin', {
+  metadata: {
+    namespace: ns.metadata.name,
+    annotations: commonAnnotations,
+  },
+  rules: [
+    {
+      apiGroups: [''],
+      resources: [
+        'pods',
+        'pods/portforward',
+        'secrets',
+        'services',
+        'persistentvolumeclaims',
+        'configmaps',
+        'deployments',
+      ],
+      verbs: ['get', 'list', 'watch', 'create', 'update', 'delete'],
+    },
+    {
+      apiGroups: [''],
+      resources: ['pods/log', 'logs'],
+      verbs: ['get', 'list', 'watch', 'create'],
+    },
+    {
+      apiGroups: ['extensions', 'apps'],
+      resources: ['replicasets', 'deployments'],
+      verbs: ['get', 'list', 'watch', 'create', 'update', 'delete'],
+    },
+  ],
+})
+
+new k8s.rbac.v1.RoleBinding('payments-full-access', {
+  metadata: {
+    namespace: ns.metadata.name,
+    annotations: commonAnnotations,
+  },
+  subjects: [
+    {
+      kind: 'Group',
+      name: 'payments-developers', // TODO: Should point at a Group or User based on the assumed IAM Role.
+    },
+  ],
+  roleRef: {
+    kind: 'Role',
+    name: r.metadata.name,
+    apiGroup: 'rbac.authorization.k8s.io',
+  },
+})
+
 // App management - to be controlled by developers
 const deployment = new k8s.apps.v1.Deployment(
   'payment-provider',
